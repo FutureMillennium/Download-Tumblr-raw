749f0f2fc7c095b04cc458a652fdc201c3186e46
Zdeněk Gromnica <futuremillennium@gmail.com>
2025-07-25 17:03:05 +0200

    + Download image, Download link

--- extension/background.js
+++ extension/background.js
@@ -16,9 +16,37 @@ function DownloadTumblrRaw(e, tab) {
 	});
 }
 
+function Download(e, tab) {
+	var url;
+
+	if (e.hasOwnProperty('srcUrl'))
+		url = e.srcUrl;
+	else if (e.hasOwnProperty('linkUrl'))
+		url = e.linkUrl;
+	else
+		return;
+
+	chrome.downloads.download({
+		url: url
+	});
+}
+
 chrome.contextMenus.create({
 	title: "Download raw image",
 	contexts: ['image'],
 	onclick: DownloadTumblrRaw,
 	targetUrlPatterns: ['*://*.tumblr.com/*'],
+});
+
+chrome.contextMenus.create({
+	title: "Download image",
+	contexts: ['image'],
+	onclick: Download,
+});
+
+chrome.contextMenus.create({
+	title: "Download link",
+	contexts: ['link'],
+	onclick: Download,
+	//targetUrlPatterns: ['*://*/*.png', '*://*/*.jpg', '*://*/*.jpeg' , '*://*/*.jpe'],
 });
